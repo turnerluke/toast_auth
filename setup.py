3313308d348e83cc9beb137189e3cc20783bc55d
turner <turnermluke@gmail.com>
2023-05-05 14:51:27 -0500

    Update.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='toast_auth',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Class object to help facilitate authentication with Toast API.',
     url='https://github.com/turnerluke/toast_auth',
